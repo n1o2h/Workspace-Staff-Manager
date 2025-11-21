78a3334ed29c6203376ede99965f9591b9cd3402
n1o2h <aythmadnhylt@gmail.com>
2025-11-21 17:02:39 +0100

    modification du local storage

--- scripts/employeScript.js
+++ scripts/employeScript.js
@@ -5,10 +5,11 @@ function loadDataEmplyer() {
       renderCardsEmplyers(employerList,"list-employe");
 }
 
-function getDataEmployersFromLocalStorageIfExist(keyData) {
+async function getDataEmployersFromLocalStorageIfExist(keyData) {
       let oldData = localStorage.getItem(keyData); //all time old data be null
       if (oldData == null || oldData == undefined){
-            loadDataJson("../data/employe.json");
+            const data = await loadDataJson("../data/employe.json");
+            localStorage.setItem(keyData, JSON.stringify(data));
             oldData = localStorage.getItem(keyData);
       }
       return JSON.parse(oldData);
@@ -672,7 +673,7 @@ function remplirToutesZonesExemple() {
             <span class="badge bg-light text-dark"><span id="nbrReception" >${reception.length}</span>/${zoneCapacities.reception}</span>`
             renderCardsEmplyers(reception, "salleReception")
       });
-      
+
       document.getElementById("serveursBtn").addEventListener("click", renderCardsEmplyers(salleServeurs, "salleServeurs"))
       document.getElementById("securiteBtn").addEventListener("click", renderCardsEmplyers(salleSecurite, "salleSecurite"))
       document.getElementById("personnelBtn").addEventListener("click", renderCardsEmplyers(sallePersonnel, "sallePersonnel"))
